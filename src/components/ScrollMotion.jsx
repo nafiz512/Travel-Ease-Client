d38100ef40466e5b2d901a9086815c8982fb4854
nafiz512 <nafizmuntasir512@gmail.com>
2025-11-11 11:13:09 +0600

    added motion components

---
+++ src/components/ScrollMotion.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { motion, useScroll } from "framer-motion";
+const ScrollMotion = () => {
+    const { scrollYProgress } = useScroll();
+
+    return (
+        <motion.div
+            id="scroll-indicator"
+            style={{
+                scaleX: scrollYProgress,
+                originX: 0,
+                zIndex: 1000,
+            }}
+            className=" bg-blue-500 top-18 left-0 right-0 h-1 fixed "
+        />
+    );
+};
+
+export default ScrollMotion;
