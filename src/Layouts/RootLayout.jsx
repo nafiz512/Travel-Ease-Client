4b88552489d147b98ad379ee657bfcfdcd829c7a
nafiz512 <nafizmuntasir512@gmail.com>
2025-11-11 11:12:16 +0600

    added motion on outlets

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import Navbar from "../components/Navbar";
 import { Toaster } from "react-hot-toast";
+import { motion, AnimatePresence } from "framer-motion";
 
 const RootLayout = () => {
+    const location = useLocation();
+
     return (
         <div className="">
             <Toaster position="top-center" reverseOrder={false} />
@@ -11,7 +14,17 @@ const RootLayout = () => {
                 <Navbar />
             </div>
             <main>
-                <Outlet />
+                <AnimatePresence mode="wait">
+                    <motion.div
+                        key={location.pathname}
+                        initial={{ y: 10, opacity: 0 }}
+                        animate={{ y: 0, opacity: 1 }}
+                        exit={{ y: -10, opacity: 0 }}
+                        transition={{ duration: 0.1 }}
+                    >
+                        <Outlet />
+                    </motion.div>
+                </AnimatePresence>
             </main>
         </div>
     );
